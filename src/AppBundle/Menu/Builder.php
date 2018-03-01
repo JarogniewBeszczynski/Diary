38c856391311601d217706dbb648abfd1e5f2009
jarogniewbeszczynski <j.beszczynski@gmail.com>
2018-03-01 14:04:41 +0100

    first

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Menu;
+
+
+use Knp\Menu\MenuFactory;
+
+class Builder
+{
+    public function mainMenu(MenuFactory $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->addChild('Home', array(
+            'route' => 'homepage'
+        ));
+
+        return $menu;
+    }
+    
+}
